ebbb531d06f2f691f9e2d1d48e97c0dbe7ba4fcb
CUG16 <chaitali.skn@gmail.com>
2024-05-11 14:31:17 +1000

    fix: changed path

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Her High Tone
+      </header>
+      This site is in progress
+    </div>
+  );
+}
+
+export default App;
